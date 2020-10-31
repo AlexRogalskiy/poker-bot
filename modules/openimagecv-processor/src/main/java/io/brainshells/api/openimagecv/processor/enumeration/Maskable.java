b6d1a3b87230892b87f592fa9776b1703b380c3d
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-10-31 12:55:43 +0300

    Updates on enums + masks

--- modules/openimagecv-processor/src/main/java/io/brainshells/api/openimagecv/processor/enumeration/Maskable.java
+++ modules/openimagecv-processor/src/main/java/io/brainshells/api/openimagecv/processor/enumeration/Maskable.java
@@ -34,10 +34,25 @@ public interface Maskable extends Supplier<List<Pattern>> {
      * @param patterns initial input array of {@link String} patterns
      * @return collection of pattern masks
      */
-    static List<Pattern> toPatterns(final String... patterns) {
+    static List<Pattern> toMask(final String... patterns) {
+        return toMask("", "", patterns);
+    }
+
+    /**
+     * Returns {@link List} of {@link Pattern} masks
+     *
+     * @param prefix   initial input {@link String} prefix
+     * @param suffix   initial input {@link String} suffix
+     * @param patterns initial input array of {@link String} patterns
+     * @return collection of pattern masks
+     */
+    static List<Pattern> toMask(final String prefix,
+                                final String suffix,
+                                final String... patterns) {
         return Arrays.stream(patterns)
             .filter(Objects::nonNull)
             .map(String::trim)
+            .map(value -> String.join("", prefix, value, suffix))
             .map(Pattern::compile)
             .collect(Collectors.toList());
     }
