755fa4864704022fd4c8d6c5b8e40470994af6a2
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-11-14 16:59:25 +0300

    Updates on card buffered image

--- modules/openimagecv-processor/src/main/java/io/brainshells/api/openimagecv/processor/utils/ImageUtils.java
+++ modules/openimagecv-processor/src/main/java/io/brainshells/api/openimagecv/processor/utils/ImageUtils.java
@@ -5,8 +5,14 @@ import io.brainshells.api.openimagecv.processor.model.Range;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -15,9 +21,108 @@ import java.nio.file.Path;
 import java.util.Base64;
 import java.util.function.UnaryOperator;
 
+import static java.awt.RenderingHints.KEY_INTERPOLATION;
+import static java.awt.RenderingHints.VALUE_INTERPOLATION_BILINEAR;
+import static java.awt.Transparency.OPAQUE;
+import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
+import static java.lang.Math.max;
+import static javax.imageio.ImageIO.read;
+
 @Slf4j
 @UtilityClass
 public class ImageUtils {
+
+    public static BufferedImage toBufferedImage(final byte[] content) throws IOException {
+        return read(new ByteArrayInputStream(content));
+    }
+
+    public static byte[] toPng(final BufferedImage image) throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", output);
+        return output.toByteArray();
+    }
+
+    public static byte[] toJpg(final BufferedImage image) throws IOException {
+        // Start with a white layer to have images with an alpha layer handled correctly.
+        final BufferedImage newBufferedImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+        newBufferedImage.createGraphics().drawImage(image, 0, 0, Color.WHITE, null);
+
+        // Manually get the ImageWriter to be able to adjust quality
+        final ImageWriter writer = ImageIO.getImageWritersBySuffix("jpg").next();
+        ImageWriteParam imageWriterParam = writer.getDefaultWriteParam();
+        imageWriterParam.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+        imageWriterParam.setCompressionQuality(1f);
+
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        writer.setOutput(new MemoryCacheImageOutputStream(output));
+        writer.write(null, new IIOImage(newBufferedImage, null, null), imageWriterParam);
+        writer.dispose();
+
+        return output.toByteArray();
+    }
+
+    public static BufferedImage cropImage(final BufferedImage image, final int desiredWidth, final int desiredHeight) {
+        final boolean cropHorizontally = (image.getWidth() > desiredWidth);
+
+        final int x = cropHorizontally ? (image.getWidth() - desiredWidth) / 2 : 0;
+        final int y = cropHorizontally ? 0 : (image.getHeight() - desiredHeight) / 2;
+
+        return image.getSubimage(x, y, desiredWidth, desiredHeight);
+    }
+
+    /*
+     * Examples of aspect ratios:
+     * 1:1 = 1.0 (will delegate to cropToSquareImage())
+     * 4:3 = 1.33333
+     * 3:2 = 1.5
+     * 16:9 = 1.77778
+     */
+    public static BufferedImage cropImage(BufferedImage image, double desiredAspectRatio) {
+        if (desiredAspectRatio == 1.0) {
+            return cropToSquareImage(image);
+        }
+
+        double currentAspectRatio = image.getWidth() * 1.0 / image.getHeight();
+
+        if (currentAspectRatio == desiredAspectRatio) {
+            return image;
+        }
+
+        boolean cropHorizontally = (currentAspectRatio > desiredAspectRatio);
+
+        int desiredWidth = cropHorizontally ? (int) (image.getHeight() * desiredAspectRatio) : image.getWidth();
+        int desiredHeight = cropHorizontally ? image.getHeight() : (int) (image.getWidth() / desiredAspectRatio);
+
+        return cropImage(image, desiredWidth, desiredHeight);
+    }
+
+    public static BufferedImage cropToSquareImage(BufferedImage image) {
+        boolean cropHorizontally = (image.getWidth() > image.getHeight());
+
+        int desiredSize = cropHorizontally ? image.getHeight() : image.getWidth();
+
+        return cropImage(image, desiredSize, desiredSize);
+    }
+
+    public static BufferedImage progressiveBilinearDownscale(BufferedImage image, int desiredWidth, int desiredHeight) {
+        BufferedImage rescaledImage = image;
+
+        while (rescaledImage.getWidth() > desiredWidth || rescaledImage.getHeight() > desiredHeight) {
+            int nextWidth = max(rescaledImage.getWidth() / 2, desiredWidth);
+            int nextHeight = max(rescaledImage.getHeight() / 2, desiredHeight);
+
+            BufferedImage nextScaledImage = new BufferedImage(nextWidth, nextHeight, image.getTransparency() == OPAQUE ? TYPE_INT_RGB : TYPE_INT_ARGB);
+            Graphics2D graphics = nextScaledImage.createGraphics();
+            graphics.setRenderingHint(KEY_INTERPOLATION, VALUE_INTERPOLATION_BILINEAR);
+            graphics.drawImage(rescaledImage, 0, 0, nextWidth, nextHeight, null);
+            graphics.dispose();
+
+            rescaledImage = nextScaledImage;
+        }
+
+        return rescaledImage;
+    }
 
     /**
      * Gets the content of an image as a Base64 encoded string.
